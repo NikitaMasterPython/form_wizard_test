f849816a0845c391b9f6b65e02ffbd9e25660aa6
NikitaMasterPython <nikitagooogl@gmail.com>
2025-07-12 20:40:08 +0300

    end copypast2

--- src/handliers/start.py
+++ src/handliers/start.py
@@ -11,6 +11,7 @@ from aiogram.types import Message
 
 from src.sevices.audio_converter.voice_to_text import VoiceToTextService
 from src.sevices.lim_giga.giga import GIGAChatService
+from src.sevices.templates.initial_template_preparation import InitialTemplatePreparation
 
 start_router = Router()
 
@@ -34,12 +35,20 @@ async def cmd_start_3(message: Message):
 async def get_audio_messages(message: Message):
     """Обработка голосовых сообщений"""
     message_text = await VoiceToTextService.parce_voice_message(message)
-    await message.answer(message_text)
+    answer_llm = await GIGAChatService.request_function(message_text, message.from_user.id)
+    await message.answer(answer_llm)
+
+
+@start_router.message(F.document)
+async def get_docx_messages(message: Message):
+    if message.content_type == types.ContentType.DOCUMENT:
+        message_text = await InitialTemplatePreparation.parce_document(message)
+        await message.answer(message_text)
 
 
 @start_router.message()
 async def get_all_messages(message: Message):
     """Обработка всех непонятных текстовых сообщений"""
     if message.content_type == types.ContentType.TEXT:
-        answer_llm = await GIGAChatService.request(message.text)
+        answer_llm = await GIGAChatService.request_function(message.text, message.from_user.id)
         await message.answer(answer_llm)
