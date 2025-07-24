86d6ac417618c789d9308c3c4deefee0e4d38085
NikitaMasterPython <nikitagooogl@gmail.com>
2025-07-24 22:01:42 +0300

    Rename test file from tees_test.py to test_tees.py

--- form_wizard_test/src/sevices/constants.py
+++
@@ -1,8 +0,0 @@
-import re
-import string
-
-TEMPLATE_CONSTANT = re.compile(r"{{[\w, \d]*}}")
-TEMPLATE_TITLE = re.compile(r"<<.*>>")
-SPLIT_LITERAL = "}} -"
-REPLACE_LITERAL = "{{"
-TEMPLATE_KEY = string.Template("{{$key}}")
